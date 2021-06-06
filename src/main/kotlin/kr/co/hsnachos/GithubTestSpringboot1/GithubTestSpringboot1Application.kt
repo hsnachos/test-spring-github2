999f5e016815c1072baeae12ee42135b0681d765
hsnachos <hsnachos@gmail.com>
2021-06-06 12:40:30 +0900

    test2

---
+++ src/main/kotlin/kr/co/hsnachos/GithubTestSpringboot1/GithubTestSpringboot1Application.kt
@@ -0,0 +1,11 @@
+package kr.co.hsnachos.GithubTestSpringboot1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubTestSpringboot1Application
+
+fun main(args: Array<String>) {
+	runApplication<GithubTestSpringboot1Application>(*args)
+}
