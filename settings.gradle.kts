972d371e64a1aba6f6693fc9554aa8841e180549
hsnachos <hsnachos@gmail.com>
2021-06-06 11:57:31 +0900

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "GithubTestSpringboot1"
---
+++ src/test/kotlin/kr/co/hsnachos/GithubTestSpringboot1/GithubTestSpringboot1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.hsnachos.GithubTestSpringboot1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubTestSpringboot1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
